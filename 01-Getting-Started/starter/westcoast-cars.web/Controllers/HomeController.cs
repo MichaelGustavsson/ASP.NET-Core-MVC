6db8482136bdb3877774fe53014932ef38dda577
MichaelGustavsson <michael.gustavsson@softtech-dev.se>
2023-01-26 19:01:00 +0100

    Initial Create

---
+++ 01-Getting-Started/starter/westcoast-cars.web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace westcoast_cars.web.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
